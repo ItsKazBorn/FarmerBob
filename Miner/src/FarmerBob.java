537ccdaa9f6aba9ad2087abd7c2a04aca786905e
Kaz Born <itskazborn@gmail.com>
2021-08-19 20:13:46 -0300

    Funções de Estados do FarmerBob

--- Miner/src/FarmerBob.java
+++ Miner/src/FarmerBob.java
@@ -16,8 +16,9 @@ public class FarmerBob {
     private int fatigue = 0;
     private int maxFatigue = 100;
 
+    private int bankMoney = 0;
     private int pocketGold = 0;
-    private int maxPocketGold = 100;
+    private int maxPocketGold = 50;
 
     public int getThirst() {
         return thirst;
@@ -49,20 +50,37 @@ public class FarmerBob {
     public State CheckState () {
         return state;
     }
+    private void ChangeState (State newState) {
+        state = newState;
+    }
 
     public void Mine () {
-
+        pocketGold += 1;
+        fatigue += 1;
+        thirst -= 5;
+        if (thirst <= 0) { ChangeState(State.QuenchThirst); }
+        if (pocketGold >= maxPocketGold) { ChangeState(State.VisitBankAndDepositGold); }
     }
-    
-    public void Rest () {
 
+    public void Rest () {
+        fatigue -= 1;
+        if (fatigue <= 0) { ChangeState(State.EnterMineAndDigForNugget); }
     }
 
     public void DepositGold () {
-
+        pocketGold -= 10;
+        bankMoney += 10;
+        if (pocketGold <= 0)  {
+            if (fatigue < maxFatigue) {
+                ChangeState(State.EnterMineAndDigForNugget);
+            }
+            else { ChangeState(State.GoHomeAndSleepTilRested); }
+        }
     }
 
     public void Drink () {
-
+        thirst += 20;
+        pocketGold -= 1;
+        if (thirst >= maxThirst) { ChangeState(State.EnterMineAndDigForNugget); }
     }
 }
