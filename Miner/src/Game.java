ccb88d04a422208915cc5fd7d3bf528ef6fa8319
Kaz Born <itskazborn@gmail.com>
2021-08-19 20:23:34 -0300

    State Machine Game Class

--- Miner/src/Game.java
+++ Miner/src/Game.java
@@ -1,9 +1,31 @@
 public class Game {
 
-    private FarmerBob bob = new FarmerBob();
-
     public static void main(String[] args) {
+        FarmerBob bob = new FarmerBob();
 
+        int action = 4;
+
+        while (true)
+        {
+            switch (bob.state)
+            {
+                case GoHomeAndSleepTilRested:
+                    bob.Rest();
+                    System.out.println("Acabei de acordar, estou disposto novamente para mais um dia de trabalho!");
+                    break;
+                case EnterMineAndDigForNugget:
+                    bob.Mine();
+                    System.out.println("Minerando estou, minerando estou.");
+                    break;
+                case QuenchThirst:
+                    bob.Drink();
+                    System.out.println("Estou matando aquela sede danada.");
+                    break;
+                case VisitBankAndDepositGold:
+                    bob.DepositGold();
+                    System.out.println("Uma pedra mineirada para mim, uma moeda para o banco.");
+            }
+        }
     }
 
 }
