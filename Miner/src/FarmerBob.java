2c507ffefa3f26409e1b16bd926acb8c6b3cf47b
Kaz Born <itskazborn@gmail.com>
2021-08-19 19:37:31 -0300

    FarmerBob stats created

---
+++ Miner/src/FarmerBob.java
@@ -0,0 +1,47 @@
+public class FarmerBob {
+    
+    private int thirst = 100;
+    private int maxThirst = 100;
+
+    private int fatigue = 0;
+    private int maxFatigue = 100;
+
+    private int pocketGold = 0;
+    private int maxPocketGold = 100;
+
+    public int getThirst() {
+        return thirst;
+    }
+
+    public void setThirst(int thirst) {
+        this.thirst = thirst;
+    }
+
+    public int getFatigue() {
+        return fatigue;
+    }
+
+    public void setFatigue(int fatigue) {
+        this.fatigue = fatigue;
+    }
+
+    public int getPocketGold() {
+        return pocketGold;
+    }
+
+    public void setPocketGold(int pocketGold) {
+        this.pocketGold = pocketGold;
+    }
+
+    public int getMaxThirst() {
+        return maxThirst;
+    }
+
+    public int getMaxFatigue() {
+        return maxFatigue;
+    }
+
+    public int getMaxPocketGold() {
+        return maxPocketGold;
+    }
+}
