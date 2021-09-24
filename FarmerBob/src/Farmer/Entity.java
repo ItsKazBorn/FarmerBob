f0b4c32770036bfbe737e778c35a352f13ace133
Kaz Born <itskazborn@gmail.com>
2021-09-23 21:22:27 -0300

    Entity ID

--- FarmerBob/src/Farmer/Entity.java
+++ FarmerBob/src/Farmer/Entity.java
@@ -4,13 +4,19 @@ import Message.Message;
 import States.StateMachine;
 
 public abstract class Entity {
+
+    private static int nextID = 0;
+
+    private int id;
+
     private String name;
 
     protected StateMachine stateMachine;
 
     public Entity(String name)  {
         this.name  = name;
-
+        this.id = nextID;
+        nextID += 1;
         EntityManager.getInstance().registerEntity(this);
     }
 
@@ -22,5 +28,9 @@ public abstract class Entity {
 
     public boolean handleMessage(Message msg) {
         return stateMachine.handleMessage(msg);
+    }
+
+    public int getId () {
+        return id;
     }
 }
